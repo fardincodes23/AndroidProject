655d8a895f6793b2d7101ec25e4b6f6784a159e7
fardinRafat-admin <frafat@hollandcollege.com>
2026-03-21 01:55:24 -0300

    SPRINT 3: Migration from XML layout to Jetpack Compose

--- project/pefumeShop/app/build.gradle.kts
+++ project/pefumeShop/app/build.gradle.kts
@@ -18,6 +18,12 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 
     buildTypes {
         release {
@@ -46,4 +52,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
+    implementation(composeBom)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.activity:activity-compose:1.8.2")
 }
